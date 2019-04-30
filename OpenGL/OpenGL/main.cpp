54dbddfc4021fc8eebf7fba184b14c67ebde5962
Pluto <596305774@qq.com>
2019-04-30 15:16:31 +0800

    The main project

---
+++ OpenGL/OpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL
+//
+//  Created by PLUTO on 2019/4/30.
+//  Copyright © 2019 PLUTO. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
